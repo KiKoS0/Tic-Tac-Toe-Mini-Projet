c257ac29a15ecce38ce2934bacef9c1ca4dcb4b3
KiKoS0 <riadh.kikos123@hotmail.fr>
2018-11-12 21:56:17 +0100

    Fix typo

--- joueur.py
+++ joueur.py
@@ -23,8 +23,8 @@ class Joueur:
             pion (string): La forme du pion choisi (ou affecté) par le joueur ("O" ou "X")
         """
 
-        assert isinstance(nom, str), "Joeur: nom doit être une chaîne de caractères."
-        assert isinstance(type, str), "Joeur: type doit être une chaîne de caractères."
+        assert isinstance(nom, str), "Joueur: nom doit être une chaîne de caractères."
+        assert isinstance(type, str), "Joueur: type doit être une chaîne de caractères."
         assert type in ["Personne", "Ordinateur"], "Joueur: type doit être 'Personne' ou 'Ordinateur'."
         assert isinstance(pion, str), "Joueur: pion doit être une chaîne de caractères."
         assert pion in ["O", "X"], "Joueur: pion doit être 'O' ou 'X'."
