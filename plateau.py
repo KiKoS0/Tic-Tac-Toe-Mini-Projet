7bb7628e5374538e19f9045eaa2f0a2e6682d7ce
KiKoS0 <riadh.kikos123@hotmail.fr>
2018-11-17 18:26:41 +0100

    Add win conditions and implement its function

--- plateau.py
+++ plateau.py
@@ -137,7 +137,10 @@ class Plateau:
         assert isinstance(pion, str), "Plateau: pion doit être une chaîne de caractères."
         assert pion in ["O", "X"], "Plateau: pion doit être 'O' ou 'X'."
 
-        pass
+        regles_vic = [[(0,0),(0,1),(0,2)],[(1,0),(1,1),(1,2)],[(2,0),(2,1),(2,2)],
+                      [(0,0),(1,0),(2,0)],[(0,1),(1,1),(2,1)],[(0,2),(1,2),(2,2)],
+                      [(0,0),(1,1),(2,2)],[(0,2),(1,1),(2,0)]]
+        return any(all(self.cases[j].est_pion(pion) for j in regle) for regle in regles_vic)
 
     def choisir_prochaine_case(self, pion):
         """
